68d643f0ab518ca1fe02a41eb0aad79171dc9811
ger0 <mrgero155@gmail.com>
2022-11-23 18:53:59 +0100

    2022PC

---
+++ .config/fish/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `userpath` on 2022-01-26 21:35:07
+set PATH $PATH /home/gero/.local/bin
+pyenv init - | source
